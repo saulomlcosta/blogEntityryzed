e914d5446a9c00a254cd3021186d9960998eb035
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-04-12 20:48:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Data;
+using Blog.Models;
+
+using var context = new BlogDataContext();
+
+context.Users.Add(new User
+{
+    Bio = "Um Desenvolvedor",
+    Email = "saulo@gmail.com",
+    Image = "http://teste.com/images/",
+    Name = "Saulo Costa",
+    PasswordHash = "password",
+    Slug = "saulo-costa"
+});
+context.SaveChanges();
+
+var user = context.Users.FirstOrDefault();
+var post = new Post
+{
+    Author = user,
+    Body = "Meu Post",
+    Category = new Category
+    {
+        Name = "Frontend",
+        Slug = "frontend"
+    },
+    CreateDate = System.DateTime.Now,
+    Slug = "meu-post",
+    Summary = "Neste artigo vamos ...",
+    Title = "Meu artigo",
+};
+context.Posts.Add(post);
+context.SaveChanges();
